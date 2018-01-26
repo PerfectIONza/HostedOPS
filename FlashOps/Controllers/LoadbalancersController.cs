c127a7ef484c43f0608303352ff312b2db3933c7
Chase <chasepretorius@gmail.com>
2018-01-26 16:34:53 +0200

    Update

---
+++ FlashOps/Controllers/LoadbalancersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlashOps.Controllers
+{
+    public class LoadbalancersController : Controller
+    {
+        // GET: Loadbalancers
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Users = "flash.local\\chase.pretorius")]
+        public ActionResult Loadbalancer40()
+        {
+            ViewBag.Message = "https://192.168.0.40:444/";
+
+            return View();
+        }
+        [Authorize(Users = "flash.local\\chase.pretorius")]
+        public ActionResult Loadbalancer45()
+        {
+            ViewBag.Message = "https://192.168.0.45:444/";
+
+            return View();
+        }
+        [Authorize(Users = "flash.local\\chase.pretorius")]
+        public ActionResult LoadbalancerLIVE()
+        {
+            ViewBag.Message = "https://lb.flash.local:444/";
+
+            return View();
+        }
+    }
+}
---
+++ FlashOps/Controllers/ServersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlashOps.Models;
+
+namespace FlashOps.Controllers
+{
+    public class ServersController : Controller
+    {
+        // GET: Servers/Devservers
+        public ActionResult ServersDEV()
+        {
+            var server = new Server() { Name = "ServersDEV" };
+            
+            return View();
+        }
+        public ActionResult ServersLIVE()
+        {
+           
+            return View();
+        }
+        public ActionResult Loadbalancer40()
+        {
+
+            return View();
+        }
+    }
+    
+}
